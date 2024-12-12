111d0881e457463b98cc99279178a254bb98abc2
wikan.dewanto <wikanworldstar@gmail.com>
2024-12-13 00:13:13 +0700

    Adding Description for Work History

--- components/WorkHistory.js
+++ components/WorkHistory.js
@@ -8,54 +8,59 @@ const WorkHistory = () => {
   return (
     <SectionWrapper>
       <Heading>Work History</Heading>
-      {data.map(({ id, image, role, company, type, date, skills, options }) => (
-        <div className="mt-6 flex gap-x-6" key={id}>
-          <div className="relative w-[50px] h-[50px] rounded-full overflow-hidden border dark:border-zinc-700">
-            <Image
-              src={image}
-              fill
-              className="w-full h-full object-cover"
-              alt={`${role} at ${company}`}
-            />
-          </div>
-          <div className="flex-1">
-            <h2 className="text-black dark:text-white capitalize font-bold text-base">
-              {role}
-            </h2>
-            <span className="capitalize text-sm font-bold text-zinc-500">
-              {company}
-            </span>
-            <div className="flex items-center gap-x-1">
-              <span className="text-black dark:text-white capitalize text-sm font-bold">
-                {type}
-              </span>
-              <span className="text-black dark:text-white">-</span>
-              <span className="text-black dark:text-white capitalize text-sm font-bold">
-                {date}
-              </span>
+      {data.map(
+        ({ id, image, role, company, type, date, desc, skills, options }) => (
+          <div className="mt-6 flex gap-x-6" key={id}>
+            <div className="relative w-[50px] h-[50px] rounded-full overflow-hidden border dark:border-zinc-700">
+              <Image
+                src={image}
+                fill
+                className="w-full h-full object-cover"
+                alt={`${role} at ${company}`}
+              />
             </div>
-            {skills && skills.length > 0 && (
-              <div className="flex gap-2 flex-wrap my-2">
-                {skills.map(({ id, ...skill }) => (
-                  <SkillComponent key={id} skill={skill} />
-                ))}
+            <div className="flex-1">
+              <h2 className="text-black dark:text-white capitalize font-bold text-base">
+                {role}
+              </h2>
+              <span className="capitalize text-sm font-bold text-zinc-500">
+                {company}
+              </span>
+              <div className="flex items-center gap-x-1">
+                <span className="text-black dark:text-white capitalize text-sm font-bold">
+                  {type}
+                </span>
+                <span className="text-black dark:text-white">-</span>
+                <span className="text-black dark:text-white capitalize text-sm font-bold">
+                  {date}
+                </span>
               </div>
-            )}
-            {options && options.length > 0 && (
-              <ul className="mt-2">
-                {options.map((option, index) => (
-                  <li
-                    key={index}
-                    className="text-zinc-900 text-base font-semibold dark:text-zinc-400"
-                  >
-                    {option}
-                  </li>
-                ))}
-              </ul>
-            )}
+              <div className="capitalize text-sm md:text-base dark:text-white text-zinc-600 ">
+                {desc}
+              </div>
+              {skills && skills.length > 0 && (
+                <div className="flex gap-2 flex-wrap my-2">
+                  {skills.map(({ id, ...skill }) => (
+                    <SkillComponent key={id} skill={skill} />
+                  ))}
+                </div>
+              )}
+              {options && options.length > 0 && (
+                <ul className="mt-2">
+                  {options.map((option, index) => (
+                    <li
+                      key={index}
+                      className="text-zinc-900 text-base font-semibold dark:text-zinc-400"
+                    >
+                      {option}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      )}
     </SectionWrapper>
   );
 };
--- data/work.js
+++ data/work.js
@@ -6,6 +6,7 @@ const data = [
     role: "Frontend Developer",
     type: "Hybrid",
     date: "01/2024 - 09/2024",
+    desc: "Caliana, a technology startup under PT Data Nusantara Adhikarya, focuses on delivering innovative solutions for 21st-century business growth.We are committed to providing top-tier services with integrity and professionalism. By fostering collaboration with clients, partners, and investors,we strive for long-term success through innovation and teamwork.",
     skills: [
       { id: 1, name: "react JS", image: "/skills/react.png" },
       { id: 2, name: "Next JS", image: "/skills/next-js.png" },
@@ -20,6 +21,7 @@ const data = [
     company: "Ditjen Diktiristek",
     type: "On-Site",
     date: "10/2022 - 01/2024",
+    desc: "PDDIKTI is a database or collection of data containing information about higher education in Indonesia. These data include student data, lecturer data, university data, and other data related to higher education.",
     skills: [
       { id: 1, name: "react JS", image: "/skills/react.png" },
       { id: 2, name: "Next JS", image: "/skills/next-js.png" },
@@ -34,6 +36,7 @@ const data = [
     company: "Ditjen Diktiristek",
     type: "On-Site",
     date: "08/2021 - 08/2022",
+    desc: "Satudikti is a mobile-based platform that is integrated with various superior services from DITJEN DIKTIRISTEK (Directorate General of Higher Education Research and Technology) such as Kampus Merdeka, PDDikti, Kedaireka, Foreign Diploma Equalization, SIVIL, Selancar PAK, and Silemkerma. SatuDikti carries a mission to provide excellent services in the scope of higher education.",
     skills: [
       { id: 1, name: "react JS", image: "/skills/react.png" },
       { id: 2, name: "Next JS", image: "/skills/next-js.png" },
