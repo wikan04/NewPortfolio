6ac6f7a5569fc3094fcc06c5e25a125e102a3d0e
wikan.dewanto <wikanworldstar@gmail.com>
2024-12-12 23:47:50 +0700

    Adding Real Information

--- data/contact.js
+++ data/contact.js
@@ -3,31 +3,31 @@ const data = [
     id: 1,
     name: "github",
     label: "github",
-    link: "http://github.com/username",
+    link: "http://github.com/wikan04",
   },
   {
     id: 2,
     name: "linkedin",
     label: "linkedin",
-    link: "http://linkedin.com/username",
+    link: "http://linkedin.com/wikan-dewanto-martonagoro-251511200",
   },
   {
     id: 3,
     name: "youtube",
     label: "youtube",
-    link: "http://youtube.com/username",
+    link: "http://youtube.com/@RandalManvis",
   },
   {
     id: 4,
     name: "instagram",
     label: "instagram",
-    link: "http://instagram.com/username",
+    link: "http://instagram.com/wikaaaann/",
   },
   {
     id: 5,
     name: "cv",
     label: "download cv",
-    link: "/files/cv.pdf",
+    link: "/files/Wikan_Dewanto_Martonagoro_Newest Resume.pdf",
   },
 ];
 export default data;
--- data/work.js
+++ data/work.js
@@ -5,7 +5,7 @@ const data = [
     company: "Caliana Indonesia",
     role: "Frontend Web Developer",
     type: "Hybrid",
-    date: "11/2022 - present",
+    date: "01/2024 - 09/2024",
     skills: [
       { id: 1, name: "react JS", image: "/skills/react.png" },
       { id: 2, name: "Next JS", image: "/skills/next-js.png" },
@@ -19,7 +19,7 @@ const data = [
     role: "Frontend Web Developer",
     company: "Ditjen Diktiristek",
     type: "On-Site",
-    date: "10/2022 - 11/2022",
+    date: "10/2022 - 01/2024",
     skills: [
       { id: 1, name: "react JS", image: "/skills/react.png" },
       { id: 2, name: "Next JS", image: "/skills/next-js.png" },
@@ -33,7 +33,7 @@ const data = [
     role: "Frontend Web Developer",
     company: "Ditjen Diktiristek",
     type: "On-Site",
-    date: "10/2022 - 11/2022",
+    date: "08/2021 - 08/2022",
     skills: [
       { id: 1, name: "react JS", image: "/skills/react.png" },
       { id: 2, name: "Next JS", image: "/skills/next-js.png" },
